876e0d8757e058d34f5c1d1859eda2d55131f609
rtsonneveld <r.t.sonneveld@gmail.com>
2018-07-20 01:56:38 +0200

    Added behaviours/scripts in the treeview

---
+++ Assets/Scripts/OpenSpace/AI/ScriptComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using OpenSpace.AI;
+using OpenSpace.EngineObject;
+
+public class ScriptComponent : MonoBehaviour {
+
+    private Script script;
+    public string offset;
+    [TextArea(30,80)]
+    public string translatedScript;
+
+    public void SetScript(Script script, Perso perso)
+    {
+        this.script = script;
+        TranslatedScript translation = new TranslatedScript(script, perso);
+        translatedScript = translation.ToString();
+        offset = script.offset.ToString();
+    }
+}
