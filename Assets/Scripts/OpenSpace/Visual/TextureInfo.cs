080c7301659ded18534dd624b086c9b168ed14e4
byvar <39142447+byvar@users.noreply.github.com>
2018-08-09 19:52:14 +0200

    Fix transparency for all versions of Rayman 2

--- Assets/Scripts/OpenSpace/Visual/TextureInfo.cs
+++ Assets/Scripts/OpenSpace/Visual/TextureInfo.cs
@@ -47,11 +47,7 @@ namespace OpenSpace.Visual {
         public bool IsTransparent {
             get {
                 if (Settings.s.engineMode == Settings.EngineMode.R2) {
-                    if (Settings.s.platform == Settings.Platform.iOS) {
-                        return (flags & 0x100) != 0 || (flags & flags_isTransparent) != 0;
-                    } else {
-                        return (flags & (1 << 1)) != 0 || (flags & flags_isTransparent) != 0;
-                    }
+                    return (flags & 0x100) != 0 || (flags & (1 << 1)) != 0 || (flags & flags_isTransparent) != 0;
                 } else {
                     return (flags & flags_isTransparent) != 0;
                 }
