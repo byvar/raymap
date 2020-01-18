f63e5c5cb845ddaa30986e0fbdd9d07edd006153
rtsonneveld <r.t.sonneveld@gmail.com>
2020-01-18 19:41:26 +0100

    Trying to read ints/floats from scripts

---
+++ Assets/Scripts/OpenSpace/ROM/Structs/AI/PrimitiveROMStructs.cs
@@ -0,0 +1,29 @@
+﻿using OpenSpace.ROM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSpace.ROM {
+
+   public class FloatROMStruct : ROMStruct {
+
+        public float value;
+
+        protected override void ReadInternal(Reader reader)
+        {
+            value = reader.ReadSingle();
+        }
+    }
+
+    public class Int32ROMStruct : ROMStruct {
+
+        public int value;
+
+        protected override void ReadInternal(Reader reader)
+        {
+            value = reader.ReadInt32();
+        }
+    }
+}
