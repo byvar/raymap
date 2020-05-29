d0c371325942b897995c22fb93f084575bcc224c
byvar <39142447+byvar@users.noreply.github.com>
2020-05-30 01:42:40 +0200

    Improvements to AI Text Search window

--- Assets/Scripts/Editor/Windows/UnityWindowSearch.cs
+++ Assets/Scripts/Editor/Windows/UnityWindowSearch.cs
@@ -14,48 +14,64 @@ public class UnityWindowSearch : UnityWindow {
 
     public string SearchString = "";
     public List<SearchableString> Results = null;
-    Vector2 scrollPos = Vector2.zero;
 
-    [MenuItem("Raymap/Search variables and scripts...")]
+
+    private float totalyPos = 0f;
+    private Vector2 scrollPosition = Vector2.zero;
+
+    [MenuItem("Raymap/Search variables and scripts")]
 	public static void ShowWindow() {
-		GetWindow<UnityWindowSearch>(false, "Search", true);
+		GetWindow<UnityWindowSearch>(false, "Text Search", true);
 	}
 	private void OnEnable() {
-		titleContent = EditorGUIUtility.IconContent("Search");
-        titleContent.text = "Search";
+		titleContent = EditorGUIUtility.IconContent("Text Icon");
+        titleContent.text = "Text Search";
     }
 
     public void OnGUI() {
 
-        if (MapLoader.Loader == null) {
+        if (!EditorApplication.isPlaying || Settings.s == null) {
             Results = null;
         }
+        if (EditorApplication.isPlaying && Settings.s != null) {
+            float yPos = 0f;
+            Rect rect = GetNextRect(ref yPos);
+            string newSearchString = EditorGUI.TextField(rect, SearchString, EditorStyles.toolbarSearchField);
+            if (newSearchString != SearchString || Results == null) {
+                SearchString = newSearchString;
 
-        string newSearchString = GUILayout.TextField(SearchString, EditorStyles.toolbarSearchField);
-        if (newSearchString != SearchString || Results == null) {
-            SearchString = newSearchString;
-
-            Results = Search(newSearchString);
-        }
-
-        scrollPos = GUILayout.BeginScrollView(scrollPos);
-        Results.ForEach(s =>
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label(s.String, GUILayout.MaxWidth(300));
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button(s.LocationString)) {
-                EditorGUIUtility.PingObject(s.RelatedGameObject);
+                Results = Search(newSearchString);
             }
-            GUILayout.EndHorizontal();
-        });
-        GUILayout.EndScrollView();
+
+            float height = position.height - yPos;
+            if (totalyPos == 0f) totalyPos = height;
+            scrollbarShown = totalyPos > height;
+            scrollPosition = GUI.BeginScrollView(new Rect(0, yPos, EditorGUIUtility.currentViewWidth, height), scrollPosition, new Rect(0, 0, EditorGUIUtility.currentViewWidth - (scrollbarShown ? scrollbarWidth : 0f), totalyPos));
+
+            yPos = 0f;
+            Results.ForEach(s => {
+                rect = GetNextRect(ref yPos);
+                GUI.Label(rect, s.String);
+                string locStr = s.LocationString;
+                float w;
+                EditorStyles.label.CalcMinMaxWidth(new GUIContent(locStr), out w, out _);
+                w = Mathf.Min(w + 10, rect.width);
+                Rect r = new Rect(rect.x + rect.width - w, rect.y, w, rect.height);
+                if (GUI.Button(r, locStr)) {
+                    EditorGUIUtility.PingObject(s.RelatedGameObject);
+                }
+            });
+            totalyPos = yPos;
+            GUI.EndScrollView();
+        } else {
+            EditorGUILayout.HelpBox("Please start the scene to use this window.", MessageType.Info);
+        }
     }
 
     private List<SearchableString> Search(string query)
     {
+        if (!EditorApplication.isPlaying || Settings.s == null) return new List<SearchableString>();
         var results = new List<SearchableString>();
-
         return MapLoader.Loader?.searchableStrings.Where(s => s.String.ToLower().Contains(query.ToLower())).ToList() ?? new List<SearchableString>();
 
     }
