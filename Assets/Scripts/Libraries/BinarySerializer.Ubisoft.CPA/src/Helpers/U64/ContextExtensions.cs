cf3e2b98ffa69d7e0b2d6129ddb240675788b298
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-02-27 18:24:49 +0100

    Throw by default if loader is not found, update BinarySerializer

--- Assets/Scripts/Libraries/BinarySerializer.Ubisoft.CPA/src/Helpers/U64/ContextExtensions.cs
+++ Assets/Scripts/Libraries/BinarySerializer.Ubisoft.CPA/src/Helpers/U64/ContextExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace BinarySerializer.Ubisoft.CPA.U64 {
     public static class ContextExtensions {
         public static LDR_Loader GetLoader(this SerializerObject s) => s.Context.GetLoader();
-        public static LDR_Loader GetLoader(this Context c) => c.GetStoredObject<LDR_Loader>(LDR_Loader.ContextKey);
+        public static LDR_Loader GetLoader(this Context c, bool throwIfNotFound = true) => 
+            c.GetStoredObject<LDR_Loader>(LDR_Loader.ContextKey, throwIfNotFound);
     }
 }
